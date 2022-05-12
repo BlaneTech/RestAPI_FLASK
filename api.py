471cca5570b880dd47a9828c37417a6318f43fe9
Dabakh DEME <demeabdou3@gmail.com>
2022-05-12 19:23:45 +0000

    Mon premier commit Api

--- api.py
+++ api.py
@@ -1,9 +1,10 @@
+from crypt import methods
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from database import *
 
 api=Flask(__name__)
-api.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://groupe4:test123@localhost/dbcreateapi'
+api.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://groupe4:test123@localhost/projetflask'
 api.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 @api.route('/')
@@ -15,19 +16,53 @@ def hello():
 def get_all_users():
     all_users=[]
     users = Users.query.all()
-    for user in users:
-        results = {
-            "id":user.userid,
-            "name":user.name,
-            "username":user.username,
-            "email":user.email,
-            "Phone": user.phone,
-            "Website":user.website,
+    company = Company.query.all()
+    for k in range(len(users)):
+        user_results = {
+            "id":users[k].userid,
+            "name":users[k].name,
+            "username":users[k].username,
+            "email":users[k].email,
+            "Phone": users[k].phone,
+            "Website":users[k].website,
+            
         }
-        all_users.append(results)
+        all_users.append(user_results)
     return jsonify({
         "users":all_users
     })
+
+@api.route('/groupe4/api/posts', methods=["GET"])
+def get_all_posts():
+    all_posts = []
+    posts = Posts.query.all()
+    for post in posts:
+        post_results = {
+            "userId":post.userid,
+            "id":post.postid,
+            "title":post.posttitle,
+            "body":post.postbody,
+        }
+        all_posts.append(post_results)
+    return jsonify({"posts":all_posts})
+
+@api.route('/groupe4/api/users/<int:userid>/albums')
+def get_userid_albums(userid):
+    albums_user = []
+    albums = Albums.query.filter_by(userid=userid).all()
+    for album in albums:
+        all_users_albums = {
+            "userid":album.userid,
+            "id":album.albumid,
+            "albumtitle":album.albumtitle,
+        }
+
+        albums_user.append(all_users_albums)
+    return jsonify({
+        "albums":albums_user
+        })
+     
+
 
 db.init_app(api)
 api.run(host='localhost', port=8000, debug=True)
